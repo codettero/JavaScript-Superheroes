9b87d1a7ae654812955ed5eb8e6c0c4f487450ab
vanntile <valentin.ionita1201@stud.acs.upb.ro>
2018-10-23 19:02:01 +0300

    Updates after Train the Trainers

--- Day1_handsOn/script.js
+++ Day1_handsOn/script.js
@@ -10,37 +10,37 @@ let previousHero = function() {
   if (currentHero === 1) {
 
     /*
-      Merge prin fiecare erou si adauga sau elimina o tranzitie 
+      Merge prin fiecare erou si adauga sau elimina o tranzitie
       definita in CSS, care determina noua sa pozitie pe ecran
       */
-    ["hero0", "hero1", "hero2"].forEach(function(hero) {
-      document.getElementById(hero).classList.add("firsthero");
-      document.getElementById(hero).classList.remove("secondhero");
+    ["jean", "cyclops", "strange"].forEach(function(hero) {
+      document.getElementById(hero).classList.add("leftScreen");
+      document.getElementById(hero).classList.remove("mainScreen");
     });
     currentHero = 0;
   } else if (currentHero === 2) {
 
-    /* 
+    /*
       TODO 0:
       Implementati trecerea de la ultimul erou la cel de-al doilea, care va avea loc
       la apasarea butonului sageata stanga. Urmariti exemplul de mai sus.
 
       La final, nu uitati sa modificati variabila ce retine numarul eroului curent.
     */
-    
+
   }
 };
 
 let nextHero = function() {
   if (currentHero === 0) {
-    ["hero0", "hero1", "hero2"].forEach(function(hero) {
-      document.getElementById(hero).classList.add("secondhero");
-      document.getElementById(hero).classList.remove("firsthero");
+    ["jean", "cyclops", "strange"].forEach(function(hero) {
+      document.getElementById(hero).classList.add("mainScreen");
+      document.getElementById(hero).classList.remove("leftScreen");
     });
     currentHero = 1;
   } else if (currentHero === 1) {
 
-    /* 
+    /*
       TODO 1:
       Implementati trecerea de la al doilea erou la cel de-al treilea, care va avea loc
       la apasarea butonului sageata dreapta. Urmariti exemplul de mai sus.
@@ -50,7 +50,16 @@ let nextHero = function() {
   }
 };
 
-window.onload = function() {
-  currentHero = 0;
-  loadHero(currentHero);
+/*
+  TODO 2:
+  Adaugati atributul onclick si ca valoare functia nextHero() linkurilor care au clasa
+  .next, respectiv previousHero() celor cu clasa .previous.
+*/
+
+window.onkeydown = function(event) {
+    if (event.keyCode == '37') { // left arrow
+       previousHero();
+    } else if (event.keyCode == '39') { // right arrow
+       nextHero();
+    }
 }
